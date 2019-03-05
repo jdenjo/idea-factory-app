a9b2187c932aa3cfef762c900c5ab0299428b705
Dean Enjo <jdenjo@gmail.com>
2019-03-04 18:54:27 -0800

    final version for quiz

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,2 +1,25 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+// idea_bodies = document.querySelectorAll(".idea-body")
+// for (let node of idea_bodies) {
+//     node.addEventlistener("click", (event) => {
+//         node.style.display = "inline"
+//     })
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('.plus').forEach((node) =>
+          node.addEventListener('click', (event) => {
+            if (event.target.parentElement.parentElement.children[1].style.display == "none"){
+                event.target.parentElement.parentElement.children[1].style.display = "block"
+            }
+            else{
+                event.target.parentElement.parentElement.children[1].style.display = "none"
+            }
+
+            })
+    );
+  });
+
